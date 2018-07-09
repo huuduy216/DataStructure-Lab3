491be89067b03e6dc9fbc5bf05030f623f59c876
huuduy216 <duyquay123@yahoo.com.vn>
2018-07-09 13:05:14 -0400

    Revise generatePassenger
    Revise generatePassenger

--- src/edu/wit/dcsn/comp2000/queueapp/Simulation.java
+++ src/edu/wit/dcsn/comp2000/queueapp/Simulation.java
@@ -15,37 +15,35 @@ public class Simulation {
 	TrainRoute route = new TrainRoute(config.getRoute());
 	TrainSpec[] trainSpecs = config.getTrains();
 	PairedLimit[] limConfig = config.getPassengers();
+
 	int seed = config.getSeed();
+	Random rand = new Random(seed);
+
 	int tick = config.getTicks();
 	int[] stationLocation = config.getStations();
 	ArrayList<Station> stations = new ArrayList<Station>();
-	
-	//Create stations
-	for(int i : stationLocation) {
+
+	// Create stations
+	for (int i : stationLocation) {
 	    stations.add(new Station(route, i));
 	}
-	
-	Random rand = new Random(seed);
-	
-	//Main loop
+
+	// Main loop
 	for (int i = 0; i < tick; i++) {
-	    ArrayList<Passenger> newPassengers = new ArrayList<Passenger>();
+	    ArrayList<ArrayList<Passenger>> newPassengers = new ArrayList<ArrayList<Passenger>>(stations.size());
+
 	    if (i == 0) {
 		newPassengers = generatePassenger(stationLocation, route, i, limConfig[0], rand);
 	    } else {
 		newPassengers = generatePassenger(stationLocation, route, i, limConfig[1], rand);
 	    }
-	    
-	    
+
+	    for (int pos = 0; pos < stations.size(); pos++) {
+		stations.get(pos).enter(newPassengers.get(pos));
+		System.out.printf("Station %n: %s/n", pos, stations.get(pos));
+	    }
+	    System.out.println("");
 	}
-	System.out.printf("Using configuration:%n\t%s%n", Arrays.toString(trainSpecs));
-
-	System.out.println("The result is:");
-
-	for (TrainSpec aTrainSpecification : trainSpecs) {
-	    Train aTrain = new Train(route, aTrainSpecification);
-	    System.out.printf("\t%s is %s with capacity %,d%n", aTrain, aTrain.getLocation(), aTrain.getCapacity());
-	} // end foreach()
 
     } // end test driver main()
 
@@ -53,16 +51,16 @@ public class Simulation {
 	return rand.nextInt(pl.maximum - pl.minimum + 1) + pl.minimum;
     }
 
-    private static ArrayList<Passenger> generatePassenger(int[] stations, TrainRoute route, int time, PairedLimit pl,
-	    Random rand) {
-	ArrayList<Passenger> newPassengers = new ArrayList<Passenger>();
+    private static ArrayList<ArrayList<Passenger>> generatePassenger(int[] stations, TrainRoute route, int time,
+	    PairedLimit pl, Random rand) {
+	ArrayList<ArrayList<Passenger>> newPassengers = new ArrayList<ArrayList<Passenger>>(stations.length);
 	for (int i = 0; i < getNextRandom(pl, rand); i++) {
 	    int station = rand.nextInt(stations.length);
 	    Location from = new Location(route, station, Direction.NOT_APPLICABLE);
 	    station = rand.nextInt(stations.length);
-	    Location to = new Location(route, station, route.whichDirection(from.getPosition(), station));
+	    Location to = new Location(route, station, Direction.NOT_APPLICABLE);
 
-	    newPassengers.add(new Passenger(from, to, time));
+	    newPassengers.get(from.getPosition()).add(new Passenger(from, to, time));
 	}
 	return newPassengers;
     }
